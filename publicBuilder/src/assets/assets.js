3e50aa0cdba2514b87bd19db93cb0a80de0daf30
imanjila <imanjilashrestha@gmail.com>
2025-12-07 12:15:45 +0545

    complete home, project and about pages

---
+++ publicBuilder/src/assets/assets.js
@@ -0,0 +1,100 @@
+import logo from './Logo1.png'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+// import header_img from './header_img.png'
+// import brand_img from './brand_img.png'
+import project_1 from './project_1.png'
+import project_2 from './project_2.png'
+import project_3 from './project_3.png'
+import project_4 from './project_4.png'
+import project_5 from './project_5.png'
+import project_6 from './project_6.png'
+import header_img from './header_img.png'
+import company_img from './company_img.png'
+
+export const assets = {
+    logo,
+    cross_icon,
+    menu_icon,
+    header_img,
+    star_icon,
+    project_1,
+    project_2,
+    project_3,
+    project_4,
+    project_5,
+    project_6,
+    left_arrow,
+    right_arrow,
+    company_img
+}
+
+export const projectsData = [
+    {
+      title: "Skyline Haven",
+      price: "Rs. 2,50,000",
+      location: "Pokhara",
+      image: project_1
+    },
+    {
+      title: "Vista Verde",
+      price: "Rs. 2,50,000",
+      location: "Amarsingh",
+      image: project_2
+    },
+    {
+      title: "Serenity Suites",
+      price: "Rs. 2,50,000",
+      location: "Kathmandu",
+      image: project_3
+    },
+    {
+      title: "Central Square",
+      price: "Rs. 2,50,000",
+      location: "Bhaktapur",
+      image: project_4
+    },
+    {
+      title: "Vista Verde",
+      price: "Rs. 2,50,000",
+      location: "Lalitpur",
+      image: project_5
+    },
+    {
+      title: "Serenity Suites",
+      price: "$2,50,000",
+      location: "Kritipur",
+      image: project_6
+    },
+    
+  ];
+
+//   export const testimonialsData = [
+//     {
+//         name: "Donald Jackman",
+//         title: "Marketing Manager",
+//         image: profile_img_1,
+//         alt: "Portrait of Donald Jackman",
+//         rating: 5,
+//         text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+//     },
+//     {
+//         name: "Richard Nelson",
+//         title: "UI/UX Designer",
+//         image: profile_img_2,
+//         alt: "Portrait of Richard Nelson",
+//         rating: 4,
+//         text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+//     },
+//     {
+//         name: "James Washington",
+//         title: "Co-Founder",
+//         image: profile_img_3,
+//         alt: "Portrait of James Washington",
+//         rating: 5,
+//         text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+//     }
+// ];
---
+++ publicBuilder/src/components/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const About = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center container mx-auto
+    p-14 md:px-29 lg:px-32 w-full overflow-hidden"
+      id="About"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2">
+        About{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          Our Company
+        </span>
+      </h1>
+      <p className="text-gray-500 max-w-80 text-center mb-8">
+        Passionate About Properties, Dedicated to Your Vision
+      </p>
+      <div className="flex flex-col md:flex-row items-center md:items-start md:gap-20">
+        <img src={assets.company_img} alt="" className="w-full sm:w-1/2 max-w-lg"/>
+        <div className="Flex flex-col items-center md:items-start mt-10 text-gray-600">
+          <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
+            <div>
+              <p className="text-4xl font-medium text-gray-800">5</p>
+              <p>Google Stars</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-800">200+</p>
+              <p>Projects Completed</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-800">150+</p>
+              <p>Happy Families</p>
+            </div>
+            <div>
+              <p className="text-4xl font-medium text-gray-800">8</p>
+              <p>Years in Service</p>
+            </div>
+          </div>
+          <p className="my-10 max-w-lg">About 8 years ago, Er. Prakash Mahato began helping
+             relatives build their homes in Pokhara. His commitment to quality quickly
+              stood out, and soon, more families began turning to him for their projects. 
+              That’s how Public Builders and Suppliers was founded. Today, we’re a team 
+              of 10+ professionals and have proudly built over 200 homes across Pokhara.
+             We specialize in residential construction and interior design—and hundreds of 
+             families have trusted us to bring their dream homes to life.</p>
+             <button className="bg-blue-600 text-white px-8 py-2 rounded">Learn more</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
